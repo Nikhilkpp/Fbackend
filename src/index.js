e83dbeb726a56986b5c68854f00a696920140c21
Nikhilkpp <nikhilpathak781@gmail.com>
2025-05-20 12:48:29 +0530

    Setup backend database models

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed", err);
+    
+});
+
+
+
+
+
+
+/*
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_Name}`)
+        
+    } catch (error) {
+        console.error("error: ",error)
+        throw error
+        
+    }
+})()
+*/
